e74c20703a4972bf1beb4e96c082563353f31da2
MartinHurnak <hurnak.m@gmail.com>
2023-12-23 19:18:00 +0100

    Day 12 refactor

--- 12/1.py
+++ 12/1.py
@@ -2,54 +2,19 @@ with open("12/input.txt") as f:
     data = f.readlines()
 
 springs = [row.split() for row in data]
-# print(springs)
-
-
-def validate(springs, sequences):
-    damaged = 0
-    groupindex = 0
-    for spring in springs:
-        if spring == "#":
-            damaged +=1
-        if spring == '.':
-            if damaged > 0:
-                if damaged != sequences[groupindex]:
-                    #print("Error:", springs, sequences)
-                    return False
-                groupindex +=1
-                damaged = 0
-
-    if damaged > 0:
-        if groupindex != len(sequences) - 1 or damaged != sequences[groupindex]:
-            #print("Error:", springs, sequences)
-            return False
-    else:
-        if groupindex < len(sequences):
-            return False
-    print(groupindex, damaged)
-    return True
-
 
 def count_damaged(springs, sequences, index=0, groupindex=0, damaged=0, help=""):
     if index >= len(springs):
         if damaged == 0 and groupindex >= len(sequences):
-            # print("C1", help)
-
             return 1
         elif groupindex == len(sequences)-1 and damaged == sequences[groupindex]:
-            # print("C2", help)
-
             return 1
         else:
-            if validate(help, sequences):
-                raise Exception
             return 0
 
     valid = 0
     for i, spring in enumerate(springs[index:]):
         if groupindex >= len(sequences) and damaged > 0:
-            if validate(help, sequences):
-                raise Exception
             return 0
         if spring == ".":
             if damaged > 0:
